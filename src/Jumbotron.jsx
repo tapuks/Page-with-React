fcb33968904f88988b182270f5551be613e9edc9
tapuks <david_berdiell@hotmail.com>
2021-05-09 11:34:32 +0000

    change 2

---
+++ src/Jumbotron.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Jumbotron = props => {
+    return (
+        <div className="jumbotron">
+            <h1 className="display-4">{props.title}}</h1>
+            <p className="lead">{props.text}</p>
+            <p className="lead">
+                <a className="btn btn-primary btn-lg" href="#" role="button">{props.button}}</a>
+            </p>
+        </div>
+    )
+}
+Jumbotron.propTypes= {
+    title: PropTypes.string,
+    text: PropTypes.string,
+    button: PropTypes.string
+}
+export default Jumbotron;
+
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,15 +1,17 @@
 import React from "react";
-const Navbar = () => {
+import PropTypes from "prop-types";
+
+const Navbar =props => {
     return (
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <a class="navbar-brand" href="#">Start Bootstrap</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
+            <div class="collapse navbar-collapse d-flex justify-content-end" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item active">
-                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                        <a class="nav-link" href="#"><span class="sr-only">Home</span></a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#">About</a>
@@ -27,4 +29,11 @@ const Navbar = () => {
 
 }
 
+Navbar.propTypes= {
+    title: PropTypes.string,
+    link1: PropTypes.string,
+    link2: PropTypes.string,
+    link3: PropTypes.string,
+    link4: PropTypes.string
+}
 export default Navbar;
