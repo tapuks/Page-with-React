42852fb382d99efa3bf9e8db4bf742a3abd3e832
tapuks <david_berdiell@hotmail.com>
2021-05-09 10:00:38 +0000

    cahge 1

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Navbar from './Navbar';
+
+//componente principal
+const Home =()=> {
+    return <h1>Hola</h1>
+}
+// here we tell React to put our main app component <Home /> inside the DOM element with id #myApp 
+ReactDOM.render(
+  <Home />,
+  document.querySelector("#myApp")
+);
